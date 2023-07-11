5da7c8e0b5b4e71d88d0d88f62b9fd65f43973eb
Hanadoggy <78478981+Hanadoggy@users.noreply.github.com>
2023-07-11 21:35:40 +0900

    add querydsl setting
    for springboot 3.1.1
    의존성 추가 및 QEntity 경로 지정

--- src/test/java/dbwls/querydsl/QuerydslApplicationTests.java
+++ src/test/java/dbwls/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,39 @@
 package dbwls.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import dbwls.querydsl.entity.Hello;
+import dbwls.querydsl.entity.QHello;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+
+		QHello qHello = new QHello("h");
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
 	}
 
 }
